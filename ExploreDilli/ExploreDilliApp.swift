81f1b81640a03f597668602732a00cbc4bb5ce54
AnishCodes <anishhazra108@gmail.com>
2025-02-15 22:57:16 +0530

    Initial Commit

---
+++ ExploreDilli/ExploreDilliApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreDilliApp.swift
+//  ExploreDilli
+//
+//  Created by Anish Hazra on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ExploreDilliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
